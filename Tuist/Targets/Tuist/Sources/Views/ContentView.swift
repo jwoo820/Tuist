59bdb789009410159689d1a623823d96c9a923cb
jwoo820 <pjwoo820@gmail.com>
2023-02-02 15:21:27 +0900

    init project

---
+++ Tuist/Targets/Tuist/Sources/Views/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Tuist
+//
+//  Created by Park Jungwoo on 2023/02/02.
+//  Copyright © 2023 tuist.io. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("Tuist Test")
+            Image(systemName: "hand.thumbsup")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
